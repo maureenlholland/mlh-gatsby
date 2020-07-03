d9bb4d706344670c24841e96c851dce8e0f92c19
maureenlholland <maureen.holland11@gmail.com>
2020-07-03 17:21:45 -0400

    Add menu toggle open animation

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css, keyframes } from "@emotion/core"
 
 import { dark } from "./theme"
+
+// todo: break out menu toggle button, component too long
 
 const tiltDown = keyframes`
     0% {
@@ -39,72 +41,89 @@ const disappear = keyframes`
 `;
 
 const Menu = () => {
+    const [isExpanded, toggleExpanded] = useState(false)
+
     return (
         <nav>
             <button
-                onClick={() => console.log('click')}
+                aria-expanded={isExpanded}
+                onClick={() => toggleExpanded(!isExpanded)}
                 css={css`
                     display: block;
                     margin-left: auto;
                     border: 1px solid transparent;
                     background: transparent;
-                    transition: background 0.2s;
-        
-                    .menu-icon {
-                        width: 30px;
-                        stroke: ${dark.colors.accent};
-                        stroke-width: 7px;
-                        transition: stroke 0.2s;
-                    }
-
-                    .line1 {
-                        transform: translateY(-30%);
-                    }
-
-                    .line3 {
-                        transform: translateY(30%);
-                    }
-        
-                    :hover,
-                    :focus {
-                        /* background: ${dark.colors.accent}; */
-        
-                        /* .menu-icon {
-                            stroke: black;
-                        } */
-
-                        .line1 {
-                            animation: ${tiltDown} 0.5s forwards;
-                        }
-
-                        .line2 {
-                            animation: ${disappear} 0.5s forwards;
-                        }
-
-                        .line3 {
-                            animation: ${tiltUp} 0.5s forwards;
-                        }
-                    }
                 `}
             >
-                <svg className="menu-icon" viewBox="0 0 100 100">
+                <svg
+                    className={`icon icon--${isExpanded ? 'close' : 'menu'}`} 
+                    viewBox="0 0 100 100"
+                    css={css`
+                        width: 30px;
+                        display: block;
+
+                        .line {
+                            stroke: ${dark.colors.accent};
+                            stroke-width: 7px;
+                        }
+
+                        .line--1 {
+                            transform: translateY(-30%);
+                        }
+
+                        .line--3 {
+                            transform: translateY(30%);
+                        }
+
+                        &.icon--close {
+                            .line--1 {
+                                animation: ${tiltDown} 0.5s forwards;
+                            }
+
+                            .line--2 {
+                                animation: ${disappear} 0.5s forwards;
+                            }
+
+                            .line--3 {
+                                animation: ${tiltUp} 0.5s forwards;
+                            }
+                        }
+
+                        /* neither runs when both classes are uncommented, try with useEffect? something about render order? */
+                        /* &.icon--menu {
+                            .line--1 {
+                                animation: ${tiltDown} 0.5s reverse forwards;
+                            }
+
+                            .line--2 {
+                                animation: ${disappear} 0.5s reverse forwards;
+                            }
+
+                            .line--3 {
+                                animation: ${tiltUp} 0.5s reverse forwards;
+                            }   
+                        } */
+                    `}
+                >
                     {/* need to put transform-origin at center after translateY (unless element starts at center, as I'm using for workaround here) */}
-                    <line className="line1" x1="0" y1="50" x2="100" y2="50" />
-                    <line className="line2" x1="0" y1="50" x2="100" y2="50" />
-                    <line className="line3" x1="0" y1="50" x2="100" y2="50" />
+                    <line className="line line--1" x1="0" y1="50" x2="100" y2="50" />
+                    <line className="line line--2" x1="0" y1="50" x2="100" y2="50" />
+                    <line className="line line--3" x1="0" y1="50" x2="100" y2="50" />
                 </svg>
             </button>
             <ul
                 css={css`
                     position: absolute;
+                    visibility:${isExpanded ? 'visible' : 'hidden'};
                     top: 100%;
-                    right: 0%;
+                    left: ${isExpanded ? '0%' : '100%'};
                     width: 100%;
                     display: grid;
                     grid-template-rows: auto;
                     grid-template-columns: 1fr auto auto auto 1fr;
                     grid-template-areas: ". about projects contact .";
                     background: ${dark.colors.accent};
+                    transition: left 0.5s;
     
                     li {
                         padding: 5px 10px;
