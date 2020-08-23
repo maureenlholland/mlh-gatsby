181bdefac69b6e04ab724b45fac9daddaa390d75
maureenlholland <maureen.holland11@gmail.com>
2020-08-23 17:19:19 -0400

    Update about

--- src/components/about.js
+++ src/components/about.js
@@ -8,10 +8,9 @@ const About = () => (
   <Section id="about">
     <div className="box" css={css`padding: 35px 20px`}>
       <Title content="About" />
-      <p>I am extremely introverted and incredibly curious.</p>
-      <p>I graduated from the University of Toronto with an Honours Bachelor of Arts in English and Cinema Studies. I decided to try coding while under-employed and watching Silicon Valley.</p>
-      <p>The career switch fit because web development feeds both the creative and logical parts of my brain. Every day is an opportunity to build something new or fix something old.</p>
-      <p>Fun fact: I started coding when Santigold's 99&#162; album came out. I have listened to Run the Races 334 times and counting.</p>
+      <p>I decided to try coding while under-employed and watching Silicon Valley.</p>
+      <p>I had graduated from the University of Toronto with an Honours Bachelor of Arts in English and Cinema Studies. I figured the only transferable skill would be my ability to sit at computer typing for hours on end. But I had spent enough time studying stories to absorb some valuable logic and creative problem solving skills as well. I bring a lot of empathy and adaptability to my web development work.</p>
+      <p>My go-to coding music from Day 1 has been Santigold's 99&#162; album. I will listen to Run the Races on repeat. 381 times and counting.</p>
     </div>
   </Section>
 )
