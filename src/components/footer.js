98d4843b166b5bc5772647b6568e5a028e9b3d76
maureenlholland <maureen.holland11@gmail.com>
2020-08-24 08:45:42 -0400

    copyright and design credit

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ import { dark } from "./theme"
 import Title from "./title"
 import ContactList from "./contact-list";
 import ThemeToggle from "./theme-toggle";
+import { FaRegCopyright } from "react-icons/fa";
 
 
 const Footer = ({ setLightTheme, setDarkTheme }) => (
@@ -12,17 +13,29 @@ const Footer = ({ setLightTheme, setDarkTheme }) => (
     id="contact"
     className="contact"
     css={css`
-      padding: 50px 20px;
       background: ${dark.colors.backgroundAlt};
       color: ${dark.colors.main};
     `}
   >
+    <div css={css`padding: 50px 20px;`}>
     <Title content="Contact" />
-    <ContactList />
-    <ThemeToggle
-      setDarkTheme={setDarkTheme}
-      setLightTheme={setLightTheme}
-    />
+      <ContactList />
+      <ThemeToggle
+        setDarkTheme={setDarkTheme}
+        setLightTheme={setLightTheme}
+      />
+    </div>
+    <p
+      css={css`
+        font-size: 0.7rem;
+        text-align: center;
+        line-height: 1;
+        margin: 0;
+        padding: 3px;
+      `}
+    >
+      <FaRegCopyright/> 2020. Design and Profile Illustration by John David Holland.
+    </p>
   </footer>
 )
 
