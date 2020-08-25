a32331da4879fd44695bb21fb30c4e3b0f61f38b
maureenlholland <maureen.holland11@gmail.com>
2020-08-25 14:00:12 -0400

    Fix splash display after opacity transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
 export const onInitialClientRender = () => {
     document.body.className = "loading";
+    const splash = document.querySelector(".splash");
+
+    splash.addEventListener('transitionend', () => {
+        splash.style.display = "none";
+    });
+
     setTimeout(function() {
         document.body.className = "loaded";
     }, 500)
