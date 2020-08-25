8874320f8aa85dda93aa329336181b99c34e459b
maureenlholland <maureen.holland11@gmail.com>
2020-08-25 13:56:22 -0400

    Add design link

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,6 +15,17 @@ const Footer = ({ setLightTheme, setDarkTheme }) => (
     css={css`
       background: ${dark.colors.backgroundAlt};
       color: ${dark.colors.main};
+      .design-link {
+        color: ${dark.colors.main};
+        text-decoration: none;
+        border-bottom: 1px dotted ${dark.colors.main};
+        transition: color 0.2s, border-bottom 0.2s;
+        &:hover,
+        &:focus {
+          color: ${dark.colors.accent};
+          border-bottom: 1px dotted ${dark.colors.accent};
+        }
+      }
     `}
   >
     <div css={css`padding: 50px 20px;`}>
@@ -34,7 +45,7 @@ const Footer = ({ setLightTheme, setDarkTheme }) => (
         padding: 3px;
       `}
     >
-      <FaRegCopyright/> 2020. Design and Profile Illustration by John David Holland.
+      <FaRegCopyright/> 2020. Design and Profile Illustration by <a className="design-link" href="https://johnhollandstudio.com/">John Holland Studio</a>.
     </p>
   </footer>
 )
